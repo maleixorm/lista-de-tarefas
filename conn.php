bf3430ce9faab3ecc0f78c12533dc84f44b2bee7
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-15 00:22:19 -0300

    Create a connection with database

---
+++ conn.php
@@ -0,0 +1,18 @@
+<?php
+
+class Connection {
+    private $host = 'localhost';
+    private $dbname = 'php_com_pdo';
+    private $user = 'php';
+    private $pass = '123456';
+
+    public function connect() {
+        try {
+            $conn = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->user, $this->pass);
+            return $conn;
+        } catch (\PDOException $e) {
+            echo '<p>' . $e->getMessage() . '</p>';
+        }
+    }
+
+}
