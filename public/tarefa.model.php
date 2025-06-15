bfc6eac523dab32920311062bdff7d209e0690d8
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-15 00:08:16 -0300

    Create class Tarefa

---
+++ public/tarefa.model.php
@@ -0,0 +1,21 @@
+<?php
+
+class Tarefa {
+    private $id;
+    private $id_status;
+    private $tarefa;
+    private $data_cadastro;
+
+    public function __construct($id, $id_status, $tarefa, $data_cadastro)
+    {
+        
+    }
+
+    public function __get($attribute) {
+        return $this->$attribute;
+    }
+
+    public function __set($attribute, $value) {
+        $this->$attribute = $value;
+    }
+}
