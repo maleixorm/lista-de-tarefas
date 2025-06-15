a5dd77904ac9eaedd3fad8bfbb50a3a3d73b877c
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-15 00:35:22 -0300

    add attributes method and action to the form and name for the input

--- public/nova_tarefa.php
+++ public/nova_tarefa.php
@@ -37,10 +37,10 @@
 								<h4>Nova tarefa</h4>
 								<hr />
 
-								<form>
+								<form method="POST" action="../tarefa.controller.php">
 									<div class="form-group">
 										<label>Descrição da tarefa:</label>
-										<input type="text" class="form-control" placeholder="Exemplo: Lavar o carro">
+										<input type="text" class="form-control" name="task" placeholder="Exemplo: Lavar o carro">
 									</div>
 
 									<button class="btn btn-success">Cadastrar</button>
