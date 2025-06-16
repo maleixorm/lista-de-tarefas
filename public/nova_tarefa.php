a32bf624f5fe821986edf40fba2655e7cba4c997
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-16 07:47:10 -0300

    Definindo o parâmetro ação ao incluir nova tarefa

--- public/nova_tarefa.php
+++ public/nova_tarefa.php
@@ -46,10 +46,10 @@
 								<h4>Nova tarefa</h4>
 								<hr />
 
-								<form method="POST" action="../tarefa.controller.php">
+								<form method="POST" action="../tarefa.controller.php?acao=inserir">
 									<div class="form-group">
 										<label>Descrição da tarefa:</label>
-										<input type="text" class="form-control" name="task" placeholder="Exemplo: Lavar o carro">
+										<input type="text" class="form-control" name="task" placeholder="Digite aqui a tarefa...">
 									</div>
 
 									<button class="btn btn-success">Cadastrar</button>
