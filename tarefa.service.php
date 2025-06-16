2402ce2c267e75f83fb2f96d6de66f3bebbdd6cb
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-16 00:21:56 -0300

    Criando um construtor para a classe e a função de inserção

--- tarefa.service.php
+++ tarefa.service.php
@@ -1,8 +1,21 @@
 <?php
 
 class TaskService {
-    public function insert() {
+    private $conexao;
+    private $tarefa;
 
+    public function __construct(Conexao $conexao, Task $tarefa)
+    {
+        $this->conexao = $conexao->connect();
+        $this->tarefa = $tarefa;
+    }
+
+    public function insert() 
+    {
+        $query = "INSERT INTO tb_tarefas(tarefa) VALUES(:tarefa)";
+        $stmt = $this->conexao->prepare($query);
+        $stmt->bindValue(':tarefa', $this->tarefa->__get('tarefa'));
+        $stmt->execute();
     }
 
     public function select() {
