b91a80455143f8969e528a2b9d6019a93b02d7b8
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-16 00:20:46 -0300

    Recebendo e inserindo dados no banco

--- tarefa.controller.php
+++ tarefa.controller.php
@@ -1,5 +1,13 @@
 <?php
 
-echo '<pre>';
-print_r($_POST);
-echo '</pre>';
+require "./conn.php";
+require "./tarefa.model.php";
+require "./tarefa.service.php";
+
+$tarefa = new Task();
+$tarefa->__set('tarefa', $_POST['task']);
+
+$conexao = new Conexao();
+
+$tarefaService = new TaskService($conexao, $tarefa);
+$tarefaService->insert();
