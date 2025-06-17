f13a791c8ef705379628c90e74ed5ffb1f301ef9
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-17 01:54:14 -0300

    Implementando a atualização dos dados no banco

--- tarefa.controller.php
+++ tarefa.controller.php
@@ -8,7 +8,7 @@ $acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
 
 if ($acao == 'inserir') {
     $tarefa = new Task();
-    $tarefa->__set('tarefa', $_POST['task']);
+    $tarefa->__set('tarefa', $_POST['tarefa']);
 
     $conexao = new Conexao();
 
@@ -22,4 +22,16 @@ if ($acao == 'inserir') {
 
     $tarefaService = new TaskService($conexao, $tarefa);
     $tarefas = $tarefaService->select();
+} else if ($acao == 'atualizar') {
+    $tarefa = new Task();
+    $tarefa->__set('id', $_POST['id']);
+    $tarefa->__set('tarefa', $_POST['tarefa']);
+
+    $conexao = new Conexao();
+
+    $tarefaService = new TaskService($conexao, $tarefa);
+    $atualizar = $tarefaService->update();
+    if ($atualizar) {
+        header('location: /lista-de-tarefas/public/todas_tarefas.php?edicao=1');
+    }
 }
