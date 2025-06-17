f0a67bf1efedf097af65eae88b9f20c77b7ae21e
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-17 00:45:32 -0300

    Criando a função de editar tarefas

---
+++ public/js/todas_tarefas.js
@@ -0,0 +1,30 @@
+function editar(id, txt_tarefa) {
+    let form = document.createElement('form');
+    form.action = '#';
+    form.method = 'post';
+    form.className = 'row';
+
+    let inputTask = document.createElement('input');
+    inputTask.type = 'text';
+    inputTask.name = 'tarefa';
+    inputTask.className = 'form-control col-sm-9';
+    inputTask.value = txt_tarefa;
+
+    let inputId = document.createElement('input');
+    inputId.type = 'hidden';
+    inputId.className = 'id';
+    inputId.value = id;
+
+    let button = document.createElement('button');
+    button.type = 'submit';
+    button.className = 'btn btn-info col-sm-3';
+    button.innerHTML = 'Atualizar';
+
+    form.appendChild(inputTask);
+    form.appendChild(inputId);
+    form.appendChild(button);
+
+    let tarefa = document.getElementById('tarefa_'+id);
+    tarefa.innerHTML = '';
+    tarefa.insertBefore(form, tarefa[0]);
+}
